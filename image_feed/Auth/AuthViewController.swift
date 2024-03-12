b0fdd749b4238cad0810006a240ebc512f970cb2
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-03-12 18:46:14 +0100

    Реализован макет AuthViewController, добавлен AuthViewController.swift

---
+++ image_feed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  image_feed
+//
+//  Created by Aleksei Frolov on 12.03.2024.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    
+}
