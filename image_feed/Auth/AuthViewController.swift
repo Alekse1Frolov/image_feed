9d29b674a34ecd524a3bf9fbc3ff14c240c641fd
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-03-18 15:47:45 +0100

    Реализован класс WebViewViewController

--- image_feed/Auth/AuthViewController.swift
+++ image_feed/Auth/AuthViewController.swift
@@ -9,5 +9,38 @@ import UIKit
 
 final class AuthViewController: UIViewController {
     
+    private let showWebViewIdentifier = "ShowWebView"
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showWebViewIdentifier {
+            guard
+                let viewController = segue.destination as? WebViewViewController else { fatalError("Failed to prepare for \(showWebViewIdentifier)") }
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP_black_color")
+    }
 }
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
+
