10aee9a0ff2c1ccc02254849bd9f8a9362cd1632
Alekse1Frolov <aleksei.frolov@customertimes.com>
2024-03-23 18:22:56 +0100

    Реализован ProgressView

--- image_feed/Auth/WebViewViewController.swift
+++ image_feed/Auth/WebViewViewController.swift
@@ -23,6 +23,8 @@ final class WebViewViewController: UIViewController {
     
     @IBOutlet private var webView: WKWebView!
     
+    @IBOutlet private var progressView: UIProgressView!
+    
     weak var delegate: WebViewViewControllerDelegate?
     
     @IBAction private func didTapBackButton(_ sender: Any?) {
@@ -51,10 +53,46 @@ final class WebViewViewController: UIViewController {
         webView.load(request)
     }
     
+    private func updateProgress() {
+        progressView.progress = Float(webView.estimatedProgress)
+        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadAuthView()
         webView.navigationDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        webView.addObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            context: nil)
+        updateProgress()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        webView.removeObserver(
+            self,
+            forKeyPath: #keyPath(WKWebView.estimatedProgress),
+            context: nil)
+    }
+    
+    override func observeValue(
+        forKeyPath keyPath: String?,
+        of object: Any?,
+        change: [NSKeyValueChangeKey : Any]?,
+        context: UnsafeMutableRawPointer?
+    ) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            updateProgress()
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
     }
 }
 
